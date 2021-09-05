d82eceea8c8f0c8e46f7bedb8790655deb2f07f3
holychicken99 <akshit.singh20@gmail.com>
2021-09-06 01:41:48 +0530

    added premake5 config

---
+++ premake5.lua
@@ -0,0 +1,22 @@
+-- premake5.lua
+workspace "boost"
+configurations {"Debug", "Release"}
+
+project "output"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+    includedirs {"/home/alpha/Documents/BOOST/boost_1_77_0/","./include/"}
+    kind "ConsoleApp"
+    files { "src/**.cpp"}
+
+
+    filter "configurations:Debug"
+        defines {"DEBUG"}
+        -- libdirs {"../../../BOOST/boost_1_77_0/stage/lib/"}
+        -- links {"boost_regex"}
+        symbols "On"
+
+    filter "configurations:Release"
+        defines {"NDEBUG"}
+        optimize "On"
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <add.h>
+#include<sub.h>
+#include <iostream>
+
+int main()
+{
+
+    std::cout << add(1, 2) << '\n';
+}
